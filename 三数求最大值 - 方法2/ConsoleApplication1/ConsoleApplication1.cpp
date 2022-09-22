d1b7e38be6c6e4db51c97714b8dbd1879ce08152
tokiyanagi <109673296+tokiyanagi@users.noreply.github.com>
2022-09-22 09:28:12 +0800

    Add files via upload

---
+++ 三数求最大值 - 方法2/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,18 @@
+﻿#include<stdio.h>
+int main()
+{
+	int max(int x, int y);
+	int a, b, c, d,i;
+	scanf_s("%d,%d,%d", &a,& b,& c);
+			i= max(a, b);
+			d = max(i, c);
+	printf("max=%d\n", d);
+	return 0;
+}
+int max(int x, int y)
+{
+	int e;
+	if (x > y )e = x;
+	else e = y;
+	return(e);
+}
